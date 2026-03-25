a0a43a653d9774c976d1eadf3af18134cc5006a7
MahmoudMohamed26 <mahmoudkamel26300@gmail.com>
2026-03-25 11:41:22 +0200

    Implement: Notification Toaster component, User Notification toaster in the websocket messages receive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,6 +18,8 @@ import { queryKeys } from "@/lib/querykeys/queryKeys";
 import InfiniteScrollNotifications from "./InfiniteScrollNotifications";
 import { useEffect, useState } from "react";
 import echo from "@/lib/echo";
+import type { NotificationType } from "@/lib/types/notification";
+import NotificationToast from "./notification-toast";
 
 const Navbar = () => {
   const location = useLocation();
@@ -28,6 +30,8 @@ const Navbar = () => {
   const Axios = useAxios();
   const [notificationOpen, setNotificationOpen] = useState<boolean>();
   const queryClient = useQueryClient();
+  const [latestToastNotification, setLatestToastNotification] =
+    useState<NotificationType | null>(null);
 
   const notifications = data?.pages.flatMap((p) => p.notifications) ?? [];
   const unreadCount = notifications.filter((n) => !n.is_read).length;
@@ -35,7 +39,9 @@ const Navbar = () => {
   useEffect(() => {
     echo
       .private("admins.notifications")
-      .listen(".notification.created", (e: Notification) => {
+      .listen(".notification.created", (e: NotificationType) => {
+        setLatestToastNotification(e);
+
         queryClient.setQueryData(queryKeys.notifications, (old: any) => {
           if (!old) return old;
           return {
@@ -73,68 +79,72 @@ const Navbar = () => {
   };
 
   return (
-    <header className="h-14 flex items-center justify-between px-4 gap-4 bg-sidebar backdrop-blur-md border-b">
-      {/* Start side */}
-      <div className="flex items-center gap-3">
-        <SidebarTrigger className="h-8 w-8" />
-        <div className="hidden sm:block">
-          <h1 className="text-sm font-semibold leading-none">{meta.title}</h1>
-          <p className="text-xs text-zinc-500 mt-0.5">{meta.description}</p>
+    <>
+      <header className="h-14 flex items-center justify-between px-4 gap-4 bg-sidebar backdrop-blur-md border-b">
+        {/* Start side */}
+        <div className="flex items-center gap-3">
+          <SidebarTrigger className="h-8 w-8" />
+          <div className="hidden sm:block">
+            <h1 className="text-sm font-semibold leading-none">{meta.title}</h1>
+            <p className="text-xs text-zinc-500 mt-0.5">{meta.description}</p>
+          </div>
         </div>
-      </div>
 
-      {/* End side */}
-      <div className="flex items-center gap-5">
-        <DropdownMenu
-          open={notificationOpen}
-          onOpenChange={setNotificationOpen}
-        >
-          <DropdownMenuTrigger asChild>
-            <button className="relative outline-none">
-              <Bell size={20} />
-              {unreadCount > 0 && (
-                <div className="absolute -top-[10px] -end-[10px] h-4.5 w-4.5 p-0 flex items-center justify-center text-[9px] bg-main text-white border-0 rounded-full">
-                  {unreadCount > 9 ? "9+" : unreadCount}
-                </div>
-              )}
-            </button>
-          </DropdownMenuTrigger>
-
-          <DropdownMenuContent
-            align="start"
-            className="w-80 mt-[13px] rounded-none! max-h-[420px] overflow-y-auto"
+        {/* End side */}
+        <div className="flex items-center gap-5">
+          <DropdownMenu
+            open={notificationOpen}
+            onOpenChange={setNotificationOpen}
           >
-            <DropdownMenuLabel className="font-semibold flex justify-between flex-row-reverse top-0 bg-popover z-10">
-              <span>الأشعارات</span>
-              {unreadCount > 0 && (
-                <button
-                  onClick={() => handleReadAll(false)}
-                  className="text-xs hover:underline cursor-pointer"
-                >
-                  تحديد الكل كمقروء
-                </button>
-              )}
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
+            <DropdownMenuTrigger asChild>
+              <button className="relative outline-none">
+                <Bell size={20} />
+                {unreadCount > 0 && (
+                  <div className="absolute -top-[10px] -end-[10px] h-4.5 w-4.5 p-0 flex items-center justify-center text-[9px] bg-main text-white border-0 rounded-full">
+                    {unreadCount > 9 ? "9+" : unreadCount}
+                  </div>
+                )}
+              </button>
+            </DropdownMenuTrigger>
 
-            <InfiniteScrollNotifications
-              notifications={notifications}
-              hasNextPage={hasNextPage}
-              isFetchingNextPage={isFetchingNextPage}
-              fetchNextPage={fetchNextPage}
-            />
-          </DropdownMenuContent>
-        </DropdownMenu>
+            <DropdownMenuContent
+              align="start"
+              className="w-80 mt-[13px] rounded-none! max-h-[420px] overflow-y-auto"
+            >
+              <DropdownMenuLabel className="font-semibold flex justify-between flex-row-reverse top-0 bg-popover z-10">
+                <span>الأشعارات</span>
+                {unreadCount > 0 && (
+                  <button
+                    onClick={() => handleReadAll(false)}
+                    className="text-xs hover:underline cursor-pointer"
+                  >
+                    تحديد الكل كمقروء
+                  </button>
+                )}
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
 
-        {/* Avatar */}
-        <Link
-          to={`/dashboard/profile`}
-          className="w-8 h-8 rounded-full flex items-center justify-center text-xs font-bold text-white bg-main cursor-pointer hover:opacity-90 transition-opacity"
-        >
-          {user?.name.slice(0, 2)}
-        </Link>
-      </div>
-    </header>
+              <InfiniteScrollNotifications
+                notifications={notifications}
+                hasNextPage={hasNextPage}
+                isFetchingNextPage={isFetchingNextPage}
+                fetchNextPage={fetchNextPage}
+              />
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* Avatar */}
+          <Link
+            to={`/dashboard/profile`}
+            className="w-8 h-8 rounded-full flex items-center justify-center text-xs font-bold text-white bg-main cursor-pointer hover:opacity-90 transition-opacity"
+          >
+            {user?.name.slice(0, 2)}
+          </Link>
+        </div>
+      </header>
+
+      <NotificationToast notification={latestToastNotification} />
+    </>
   );
 };
 
