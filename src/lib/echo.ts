dcf217a82bfb2596fc160c93125c64921d1037b4
MahmoudMohamed26 <mahmoudkamel26300@gmail.com>
2026-08-03 16:03:57 +0300

    Update: Echo configurations

--- src/lib/echo.ts
+++ src/lib/echo.ts
@@ -1,29 +1,34 @@
-import Echo from 'laravel-echo';
-import Pusher from 'pusher-js';
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 import Cookies from "js-cookie";
 
 declare global {
-    interface Window {
-        Pusher: typeof Pusher;
-    }
+  interface Window {
+    Pusher: typeof Pusher;
+  }
 }
 
 window.Pusher = Pusher;
 
 const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: true,
-    enabledTransports: ['ws', 'wss'],
-
-    authEndpoint: `${import.meta.env.VITE_WS_URL}/broadcasting/auth`,
-    auth: {
-        headers: {
-            Authorization: `Bearer ${Cookies.get("mithaq-admin")}`,
-        },
+  broadcaster: "reverb",
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: import.meta.env.VITE_REVERB_PORT,
+  forceTLS: true,
+  enabledTransports: ["ws", "wss"],
+  authEndpoint: `${import.meta.env.VITE_WS_URL}/broadcasting/auth`,
+  auth: {
+    headers: {
+      Authorization: `Bearer ${Cookies.get("mithaq-admin")}`,
     },
+  },
 });
+
+echo
+  .private("private-admins.notifications")
+  .notification((notification: any) => {
+    console.log(notification);
+  });
 
 export default echo;
